95b816c628edf08699ee7a9510c4ab2a69d9df19
vn666 <18639881573@163.com>
2018-10-17 14:12:16 +0800

    初始化git仓库

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+
+var app = express();
+
+/*设置handlebars视图引擎*/
+var handlebars = require('express3-handlebars')
+	.create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.type('text/html');
+	res.render('home');
+});
+
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
+app.use( (req, res, next) => {
+	res.status(404);
+	res.render('404');
+});
+
+app.use( (err, req, res, next) => {
+	console.error(err);
+	res.status(500);
+	res.render('500');
+});
+
+/*定制404页面*/
+app.use( (req, res) => {
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+/*定制500页面*/
+app.use( (err, req, res, next) => {
+	console.error(err);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), () => {
+	console.info('Exprss started on http://localhost:' + app.get('port') + '; press Ctrl+C to terminate.');
+});
